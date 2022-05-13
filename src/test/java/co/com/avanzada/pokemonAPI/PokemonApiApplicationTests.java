6e7e90bc637ca772a6ba9dea0c3edd709cfc2713
ElianCelisA <elian.celisa@utadeo.edu.co>
2022-05-12 22:51:26 -0500

    initialization of the project structure

---
+++ src/test/java/co/com/avanzada/pokemonAPI/PokemonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.avanzada.pokemonAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
