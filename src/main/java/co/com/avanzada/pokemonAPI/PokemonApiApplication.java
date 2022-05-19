4b7a111ff7b6d1fa1b6a71a81aff570ff0166742
ElianCelisA <elian.celisa@utadeo.edu.co>
2022-05-19 11:07:51 -0500

    Culminación componente del Backend

--- src/main/java/co/com/avanzada/pokemonAPI/PokemonApiApplication.java
+++ src/main/java/co/com/avanzada/pokemonAPI/PokemonApiApplication.java
@@ -2,12 +2,47 @@ package co.com.avanzada.pokemonAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
 public class PokemonApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonApiApplication.class, args);
+	}
+
+}
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class MyCORSFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD, PUT");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+		chain.doFilter(req, res);
+	}
+
+	@Override
+	public void destroy() {
 	}
 
 }
